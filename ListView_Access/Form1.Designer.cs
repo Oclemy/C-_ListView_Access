84f686edbbbea674f3edfb5f9e2d02911f84d876
Oclemy <oclemmi@gmail.com>
2017-07-27 03:58:19 +0300

    Initial Commit

---
+++ ListView_Access/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace ListView_Access
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.deleteBtn = new MetroFramework.Controls.MetroButton();
+            this.updateBtn = new MetroFramework.Controls.MetroButton();
+            this.retrieveBtn = new MetroFramework.Controls.MetroButton();
+            this.addBtn = new MetroFramework.Controls.MetroButton();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.destinationTxt = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.propellantTxt = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.nameTxt = new MetroFramework.Controls.MetroTextBox();
+            this.clearBtn = new MetroFramework.Controls.MetroButton();
+            this.mListView = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(715, 350);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(141, 23);
+            this.deleteBtn.TabIndex = 20;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseSelectable = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(570, 350);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(109, 23);
+            this.updateBtn.TabIndex = 19;
+            this.updateBtn.Text = "Update";
+            this.updateBtn.UseSelectable = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // retrieveBtn
+            // 
+            this.retrieveBtn.Location = new System.Drawing.Point(715, 295);
+            this.retrieveBtn.Name = "retrieveBtn";
+            this.retrieveBtn.Size = new System.Drawing.Size(141, 23);
+            this.retrieveBtn.TabIndex = 18;
+            this.retrieveBtn.Text = "Retrieve";
+            this.retrieveBtn.UseSelectable = true;
+            this.retrieveBtn.Click += new System.EventHandler(this.retrieveBtn_Click);
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(570, 295);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(109, 23);
+            this.addBtn.TabIndex = 17;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseSelectable = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(570, 227);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(73, 19);
+            this.metroLabel3.TabIndex = 16;
+            this.metroLabel3.Text = "Destination";
+            // 
+            // destinationTxt
+            // 
+            // 
+            // 
+            // 
+            this.destinationTxt.CustomButton.Image = null;
+            this.destinationTxt.CustomButton.Location = new System.Drawing.Point(133, 1);
+            this.destinationTxt.CustomButton.Name = "";
+            this.destinationTxt.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.destinationTxt.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.destinationTxt.CustomButton.TabIndex = 1;
+            this.destinationTxt.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.destinationTxt.CustomButton.UseSelectable = true;
+            this.destinationTxt.CustomButton.Visible = false;
+            this.destinationTxt.Lines = new string[0];
+            this.destinationTxt.Location = new System.Drawing.Point(671, 227);
+            this.destinationTxt.MaxLength = 32767;
+            this.destinationTxt.Name = "destinationTxt";
+            this.destinationTxt.PasswordChar = '\0';
+            this.destinationTxt.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.destinationTxt.SelectedText = "";
+            this.destinationTxt.SelectionLength = 0;
+            this.destinationTxt.SelectionStart = 0;
+            this.destinationTxt.ShortcutsEnabled = true;
+            this.destinationTxt.Size = new System.Drawing.Size(155, 23);
+            this.destinationTxt.TabIndex = 15;
+            this.destinationTxt.UseSelectable = true;
+            this.destinationTxt.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.destinationTxt.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.Location = new System.Drawing.Point(570, 164);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(69, 19);
+            this.metroLabel2.TabIndex = 14;
+            this.metroLabel2.Text = "Propellant";
+            // 
+            // propellantTxt
+            // 
+            // 
+            // 
+            // 
+            this.propellantTxt.CustomButton.Image = null;
+            this.propellantTxt.CustomButton.Location = new System.Drawing.Point(133, 1);
+            this.propellantTxt.CustomButton.Name = "";
+            this.propellantTxt.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.propellantTxt.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.propellantTxt.CustomButton.TabIndex = 1;
+            this.propellantTxt.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.propellantTxt.CustomButton.UseSelectable = true;
+            this.propellantTxt.CustomButton.Visible = false;
+            this.propellantTxt.Lines = new string[0];
+            this.propellantTxt.Location = new System.Drawing.Point(671, 164);
+            this.propellantTxt.MaxLength = 32767;
+            this.propellantTxt.Name = "propellantTxt";
+            this.propellantTxt.PasswordChar = '\0';
+            this.propellantTxt.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.propellantTxt.SelectedText = "";
+            this.propellantTxt.SelectionLength = 0;
+            this.propellantTxt.SelectionStart = 0;
+            this.propellantTxt.ShortcutsEnabled = true;
+            this.propellantTxt.Size = new System.Drawing.Size(155, 23);
+            this.propellantTxt.TabIndex = 13;
+            this.propellantTxt.UseSelectable = true;
+            this.propellantTxt.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.propellantTxt.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(570, 95);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(45, 19);
+            this.metroLabel1.TabIndex = 12;
+            this.metroLabel1.Text = "Name";
+            // 
+            // nameTxt
+            // 
+            // 
+            // 
+            // 
+            this.nameTxt.CustomButton.Image = null;
+            this.nameTxt.CustomButton.Location = new System.Drawing.Point(133, 1);
+            this.nameTxt.CustomButton.Name = "";
+            this.nameTxt.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.nameTxt.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.nameTxt.CustomButton.TabIndex = 1;
+            this.nameTxt.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.nameTxt.CustomButton.UseSelectable = true;
+            this.nameTxt.CustomButton.Visible = false;
+            this.nameTxt.Lines = new string[0];
+            this.nameTxt.Location = new System.Drawing.Point(671, 95);
+            this.nameTxt.MaxLength = 32767;
+            this.nameTxt.Name = "nameTxt";
+            this.nameTxt.PasswordChar = '\0';
+            this.nameTxt.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.nameTxt.SelectedText = "";
+            this.nameTxt.SelectionLength = 0;
+            this.nameTxt.SelectionStart = 0;
+            this.nameTxt.ShortcutsEnabled = true;
+            this.nameTxt.Size = new System.Drawing.Size(155, 23);
+            this.nameTxt.TabIndex = 11;
+            this.nameTxt.UseSelectable = true;
+            this.nameTxt.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.nameTxt.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(662, 401);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(119, 23);
+            this.clearBtn.TabIndex = 21;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseSelectable = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // mListView
+            // 
+            this.mListView.Location = new System.Drawing.Point(23, 86);
+            this.mListView.Name = "mListView";
+            this.mListView.Size = new System.Drawing.Size(486, 338);
+            this.mListView.TabIndex = 22;
+            this.mListView.UseCompatibleStateImageBehavior = false;
+            this.mListView.SelectedIndexChanged += new System.EventHandler(this.mListView_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 457);
+            this.Controls.Add(this.mListView);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.retrieveBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.destinationTxt);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.propellantTxt);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.nameTxt);
+            this.Name = "Form1";
+            this.Text = "ListView MS Access";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton deleteBtn;
+        private MetroFramework.Controls.MetroButton updateBtn;
+        private MetroFramework.Controls.MetroButton retrieveBtn;
+        private MetroFramework.Controls.MetroButton addBtn;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroTextBox destinationTxt;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroTextBox propellantTxt;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox nameTxt;
+        private MetroFramework.Controls.MetroButton clearBtn;
+        private System.Windows.Forms.ListView mListView;
+    }
+}
+
---
+++ ListView_Access/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+
+namespace ListView_Access
+{
+    public partial class Form1 : MetroForm
+    {
+        private const string conString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:/Users/Hp/Documents/DataBases/spacecraftsDB.mdb;";
+        readonly OleDbConnection con = new OleDbConnection(conString);
+        OleDbCommand cmd;
+        OleDbDataAdapter adapter;
+        readonly DataTable dt = new DataTable();
+
+        /*
+       * CONSTRUCTOR
+       */
+        public Form1()
+        {
+            InitializeComponent();
+            this.setupListView();
+           
+        }
+        /*
+         * SETUP LISTVIEW COLUMNS AND PRPERTIES
+         */
+        private void setupListView()
+        {
+            mListView.View = View.Details;
+            mListView.FullRowSelect = true;
+            mListView.MultiSelect = false;
+            mListView.Columns.Add("ID", 30);
+            mListView.Columns.Add("Name", 150);
+            mListView.Columns.Add("Propellant", 150);
+            mListView.Columns.Add("Destination", 150);
+
+
+        }
+
+        /*
+         * INSERT INTO DB
+         */
+        private void add(string name, string propellant, string destination)
+        {
+            //SQL STMT
+            const string sql = "INSERT INTO spacecraftsTB(S_Name,S_Propellant,S_Destination) VALUES(@NAME,@PROPELLANT,@DESTINATION)";
+            cmd = new OleDbCommand(sql, con);
+
+            //ADD PARAMS
+            cmd.Parameters.AddWithValue("@NAME", name);
+            cmd.Parameters.AddWithValue("@PROPELLANT", propellant);
+            cmd.Parameters.AddWithValue("@DESTINATION", destination);
+
+            //OPEN CON AND EXEC INSERT
+            try
+            {
+                con.Open();
+                if (cmd.ExecuteNonQuery() > 0)
+                {
+                    clearTxts();
+                    MessageBox.Show(@"Successfully Inserted");
+                }
+                con.Close();
+                retrieve();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        /*
+         * FILL LISTVIEW
+         */
+        private void populate(string id, string name, string propellant, string destination)
+        {
+           //POPULATE SPACECRAFTS
+             mListView.Items.Add(new ListViewItem(new[] { id,name,propellant,destination }));
+        }
+
+        /*
+         * RETRIEVAL OF DATA
+         */
+        private void retrieve()
+        {
+            mListView.Items.Clear();
+            //SQL STATEMENT
+            String sql = "SELECT * FROM spacecraftsTB ";
+            cmd = new OleDbCommand(sql, con);
+            try
+            {
+                con.Open();
+                adapter = new OleDbDataAdapter(cmd);
+                adapter.Fill(dt);
+                //LOOP THROUGH DATATABLE
+                foreach (DataRow row in dt.Rows)
+                {
+                    populate(row[0].ToString(), row[1].ToString(), row[2].ToString(), row[3].ToString());
+                }
+
+                con.Close();
+                //CLEAR DATATABLE 
+                dt.Rows.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        /*
+          *  UPDATE DATABASE
+         */
+        private void update(int id, string name, string propellant, string destination)
+        {
+            //SQL STATEMENT
+            string sql = "UPDATE spacecraftsTB SET S_Name='" + name + "',S_Propellant='" + propellant + "',S_Destination='" + destination + "' WHERE ID=" + id + "";
+            cmd = new OleDbCommand(sql, con);
+
+            //OPEN CONNECTION,UPDATE,RETRIEVE DATAGRIDVIEW
+            try
+            {
+                con.Open();
+                adapter = new OleDbDataAdapter(cmd)
+                {
+                    UpdateCommand = con.CreateCommand()
+                };
+                adapter.UpdateCommand.CommandText = sql;
+                if (adapter.UpdateCommand.ExecuteNonQuery() > 0)
+                {
+                    clearTxts();
+                    MessageBox.Show(@"Successfully Updated");
+                }
+                con.Close();
+
+                //REFRESH DATA
+                retrieve();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        /*
+         * DELETE FROM DATABASE
+         */
+        private void delete(int id)
+        {
+            //SQL STATEMENTT
+            String sql = "DELETE FROM spacecraftsTB WHERE ID=" + id + "";
+            cmd = new OleDbCommand(sql, con);
+
+            //'OPEN CONNECTION,EXECUTE DELETE,CLOSE CONNECTION
+            try
+            {
+                con.Open();
+                adapter = new OleDbDataAdapter(cmd);
+                adapter.DeleteCommand = con.CreateCommand();
+                adapter.DeleteCommand.CommandText = sql;
+
+                //PROMPT FOR CONFIRMATION BEFORE DELETING
+                if (MessageBox.Show(@"Are you sure to permanently delete this?", @"DELETE", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+                {
+                    if (cmd.ExecuteNonQuery() > 0)
+                    {
+                        clearTxts();
+                        MessageBox.Show(@"Successfully deleted");
+                    }
+                }
+                con.Close();
+                retrieve();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+        /*
+         * CLEAR TEXTBOXES
+         */
+        private void clearTxts()
+        {
+            nameTxt.Text = "";
+            propellantTxt.Text = "";
+            destinationTxt.Text = "";
+        }
+
+
+
+        /*
+         * ADD BUTTON CLICKED
+         */
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            add(nameTxt.Text, propellantTxt.Text, destinationTxt.Text);
+
+        }
+        /*
+       * RETRIEVE BUTTON CLICKED
+       */
+        private void retrieveBtn_Click(object sender, EventArgs e)
+        {
+            retrieve();
+        }
+        /*
+         * UPDATE BUTTON CLICKED
+         */
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+
+            int selectedIndex = mListView.SelectedIndices[0];
+            if (selectedIndex != -1)
+            {
+
+                String selected = mListView.SelectedItems[0].SubItems[0].Text;
+                int id = Convert.ToInt32(selected);
+                update(id, nameTxt.Text, propellantTxt.Text, destinationTxt.Text);
+            }
+
+        }
+        /*
+        * DELETE BUTTON CLICKED
+        */
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = mListView.SelectedIndices[0];
+            if (selectedIndex != -1)
+            {
+                String selected = mListView.SelectedItems[0].SubItems[0].Text;
+                int id = Convert.ToInt32(selected);
+                delete(id);
+            }
+        }
+
+        /*
+         * CLEAR BUTTON CLICKED
+         */
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            mListView.Items.Clear();
+            clearTxts();
+        }
+
+        /*
+         * LISTVIEW SELECTION CHANGED
+         */
+        private void mListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int selectedIndex = mListView.SelectedIndices[0];
+                if (selectedIndex != -1)
+                {
+                    if (mListView.SelectedItems[0].SubItems[0].Text != null)
+                    {
+                        string name = mListView.SelectedItems[0].SubItems[1].Text;
+                        string propellant = mListView.SelectedItems[0].SubItems[2].Text;
+                        string destination = mListView.SelectedItems[0].SubItems[3].Text;
+                        
+                        nameTxt.Text = name;
+                        propellantTxt.Text = propellant;
+                        destinationTxt.Text = destination;
+                    }
+
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+
+            }
+        }
+
+
+    }
+}
